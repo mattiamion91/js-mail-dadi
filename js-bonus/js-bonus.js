3690a79d9d8200caa3dd86c95e9d6ee595bca6b7
Mattia Mion <mattia.mion@gmail.com>
2025-12-04 17:31:23 +0100

    feat: adding bonus folder

---
+++ js-bonus/js-bonus.js
@@ -0,0 +1,15 @@
+/*
+BONUS (a grande richiesta... 
+
+:raised_hands:
+Clicca per scoprire di più
+ , in questo caso quindi create una terza cartella sempre nella stessa repo che chimate "bonus"):
+JSnack 2
+L’utente inserisce due parole in successione, con due prompt. 
+Il software stampa prima la parola più corta, poi la parola più lunga;,
+JSnack 5
+Crea un array vuoto. 
+Chiedi per 6 volte all’utente di inserire un numero, 
+se è dispari inseriscilo nell’array
+*/
+
